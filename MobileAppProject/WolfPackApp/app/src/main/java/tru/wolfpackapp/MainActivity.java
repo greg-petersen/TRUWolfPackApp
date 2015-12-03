58135c01673e8306e20a5b298bfc6b3f87022ad8
masterpal05 <ramclamp@gmail.com>
2015-12-03 12:12:02 -0800

    All activities have been linked and i was playing with the background........ because i thing the colors don't really contrast. lol looked at it for a wile and it think it started to get to me.

--- MobileAppProject/WolfPackApp/app/src/main/java/tru/wolfpackapp/MainActivity.java
+++ MobileAppProject/WolfPackApp/app/src/main/java/tru/wolfpackapp/MainActivity.java
@@ -1,18 +1,37 @@
 package tru.wolfpackapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {//monkey hello change
+public class MainActivity extends AppCompatActivity
+{//monkey hello change
+    Button go_next;//initializing the only button on the screen so far
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        go_next = (Button) findViewById(R.id.Cleck_my_flow);//initializing the go net button
+        go_next.setOnClickListener(goNext);//setting the onclick listener
+    }
+    private View.OnClickListener goNext = new View.OnClickListener()//function to call the next activity
+    {
+        @Override
+        public void onClick(View v)
+        {
+            Intent intent = new Intent (v.getContext(), splashscreen.class);
+            startActivity(intent);
+        }
+    };//i stopped here, just following our graphical flow from the draw.io
+
+    //this is java generated code below
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
