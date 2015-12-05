9db3b30dbb1c8a05a77f5f3df85b91f77755f532
darkhobbo <gregpetersen@hotmail.com>
2015-12-05 12:54:05 -0800

    Removing useless code.
    Cleaning up application flow.

--- MobileAppProject/WolfPackApp/app/src/main/java/tru/wolfpackapp/calendar.java
+++ MobileAppProject/WolfPackApp/app/src/main/java/tru/wolfpackapp/calendar.java
@@ -3,11 +3,11 @@ package tru.wolfpackapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class currentseason extends AppCompatActivity {
+public class calendar extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.currentseason);
+        setContentView(R.layout.calendar);
     }
 }
--- MobileAppProject/WolfPackApp/app/src/main/java/tru/wolfpackapp/splashscreen.java
+++ MobileAppProject/WolfPackApp/app/src/main/java/tru/wolfpackapp/splashscreen.java
@@ -3,8 +3,6 @@ package tru.wolfpackapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -30,26 +28,4 @@ public class splashscreen extends AppCompatActivity
             startActivity(intent);
         }
     };//i stopped here, just following our graphical flow from the draw.io
-
-    //this is java generated code below
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_splashscreen, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
